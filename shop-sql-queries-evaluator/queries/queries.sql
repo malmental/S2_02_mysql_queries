fec9a66193f8f72bb320f4628061ae8f2b9127a5
malmental <orlandorojasoyarzun@gmail.com>
2026-01-12 21:08:03 +0100

    queries completas

--- shop-sql-queries-evaluator/queries/queries.sql
+++ shop-sql-queries-evaluator/queries/queries.sql
@@ -108,7 +108,7 @@ SELECT f.nombre AS fabricante FROM fabricante f LEFT JOIN producto p ON f.codigo
 SELECT * FROM producto WHERE codigo_fabricante = ( SELECT codigo FROM fabricante WHERE nombre = 'Lenovo');
 
 -- 37. Retorna totes les dades dels productes que tenen el mateix preu que el producte més car del fabricant Lenovo. (Sense usar INNER JOIN).
-
+SELECT * FROM producto WHERE precio = (SELECT MAX(precio) FROM producto WHERE codigo_fabricante = (SELECT codigo FROM fabricante WHERE nombre = 'Lenovo'));
 
 -- 38. Llista el nom del producte més car del fabricant Lenovo.
 SELECT nombre FROM producto WHERE precio = (SELECT MAX(precio) FROM producto WHERE codigo_fabricante = (SELECT codigo FROM fabricante WHERE nombre = 'Lenovo'));
